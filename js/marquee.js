3e368bb8f75528e0210402b85ff31cffa6c7a038
Matan Toledano <mytole@gmail.com>
2020-11-15 17:35:44 +0200

    8th_Milestone

--- js/marquee.js
+++ js/marquee.js
@@ -1,28 +1,28 @@
 class Marquee {
-    constructor(element){
+    constructor(element) {
         this.marqElement = element;
     }
-    load(){
-        fetch('https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/stock/list').then(response=>{
-    response.json().then(data=>{
-        data.splice(50, data.length-50);
-        
-        let compSymbs = [];
-        data.forEach(comp => compSymbs.push(comp.symbol));
-        compSymbs.forEach(symbol => {
-            getProfile(symbol).then(proComp =>{
-                console.log(proComp);
-                let currLi = document.createElement("li");
-                this.marqElement.appendChild(currLi);
-                currLi.innerHTML = `<b>${symbol}</b>&nbsp${proComp.profile.price}$  ${proComp.profile.changes} <span id="${symbol}c">${proComp.profile.changesPercentage}</span>`; 
-                if(proComp.profile.changesPercentage.charAt(1)==='+') document.getElementById(`${symbol}c`).style.color = '#0ddd83';
-                    else document.getElementById(`${symbol}c`).style.color = 'red';
+    load() {
+        fetch('https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/stock/list').then(response => {
+            response.json().then(data => {
+                data.splice(50, data.length - 50);
+
+                let compSymbs = [];
+                data.forEach(comp => compSymbs.push(comp.symbol));
+                compSymbs.forEach(symbol => {
+                    getProfile(symbol).then(proComp => {
+                        console.log(proComp);
+                        let currLi = document.createElement("li");
+                        this.marqElement.appendChild(currLi);
+                        currLi.innerHTML = `<b>${symbol}</b>&nbsp${proComp.profile.price}$  ${proComp.profile.changes} <span id="${symbol}c">${proComp.profile.changesPercentage}</span>`;
+                        if (proComp.profile.changesPercentage.charAt(1) === '+') document.getElementById(`${symbol}c`).style.color = '#0ddd83';
+                        else document.getElementById(`${symbol}c`).style.color = 'red';
+                    })
+                })
+
+
             })
+
         })
-        
-        
-    })
-    
-})
     }
 }
